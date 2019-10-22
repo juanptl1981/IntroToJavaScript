c1d43114827267e1200f9d9c5ad6a48c19030e4f
Juan Torres <juanptlr@gmail.com>
2019-10-21 21:04:17 -0500

    All Files Local Machine / Index.html and Static

--- UFO-level-1/Static/js/data.js
+++ UFO-level-1/Static/js/data.js
@@ -5,7 +5,8 @@ var data = [{
     country: "us",
     shape: "circle",
     durationMinutes: "5 mins.",
-    comments: "4 bright green circles high in the sky going in circles then one bright green light at my front door."
+    comments: "4 bright green circles high in the sky going in circles then one bright green light at my front door.",
+    jp: "juan torres"
   },
   {
     datetime: "1/1/2010",
